99c61278d95598123d53a78f0301edb92311ee4b
Tony Li Xu <tonylixu@gmail.com>
2017-10-28 15:29:14 -0400

    Added add-numbers-array python file

---
+++ algorithm/add-numbers-array/README.md
@@ -0,0 +1,6 @@
+## Add each number in an array until the sum equals the rest of the array
+Questions:
+* What the maximum number of elements in this array?
+* What should the output be if the array doesn't qualify
+* Does the array contain integers only? or also decimals
+* What if the array contains characters?
---
+++ algorithm/add-numbers-array/add-numbers-array.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 5, 6, 8, 25]
+numbers2 = []
+
+length = len(numbers)
+# Found flag
+found = False
+
+for i in range(length):
+    numbers2.append(numbers.pop(0))
+    if sum(numbers2) == sum(numbers):
+        print "Number of steps is " + str(i+1)
+        found = True
+        break
+
+if not found:
+    print("Array disqualified")
