6d138a67373b3ad434e8982451f47ee384a194f3
tonylixu@gmail.com <tonylixu@gmail.com>
2017-10-31 10:48:30 -0400

    Added time and space complexity, add main function

--- algorithm/sorting/selection-sort.py
+++ algorithm/sorting/selection-sort.py
@@ -1,10 +1,14 @@
-cards = [2, 7, 4, 1, 5, 3]
-sorted_cards = []
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 
-length = len(cards)
-for i in range(length):
-    for j in range(i+1, length):
-        if cards[i] > cards[j]:
-            (cards[i], cards[j]) = (cards[j], cards[i])
+def selection_sort(array):
+    length = len(array)
+    for i in range(length):
+        for j in range(i+1, length):
+            if array[i] > array[j]:
+                (array[i], array[j]) = (array[j], array[i])
+    return array
 
-print cards
+if __name__ == "__main__":
+    cards = [2, 7, 4, 1, 5, 3]
+    print selection_sort(cards)
