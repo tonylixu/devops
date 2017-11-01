3a2c766822ad7bfab100296fe909139510015c75
tonylixu@gmail.com <tonylixu@gmail.com>
2017-11-01 10:20:29 -0400

    Updated comments

--- algorithm/1-bit-2-bit/one-or-two-bits.py
+++ algorithm/1-bit-2-bit/one-or-two-bits.py
@@ -1,9 +1,15 @@
-bits = [1, 0, 1, 0]
+bits = [1, 0, 0]
 # Calculate the array length
 length = len(bits) - 1
 i = 0
+
+# Iterate the whole array till the last element
 while i < length:
+    # If bits[i] == 0, we move 1
+    # else we move 2
+    # 
     i += bits[i] + 1
+# If we have one element left
 if i == length:
     print True
 else:
