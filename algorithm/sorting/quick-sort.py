8d862c4c4b1f053afc2ba362bb4fdd9af0618603
Tony Li Xu <tonylixu@gmail.com>
2017-11-02 22:13:31 -0400

    Added quicksort function definition

--- algorithm/sorting/quick-sort.py
+++ algorithm/sorting/quick-sort.py
@@ -1,13 +1,24 @@
 
+def quicksort(array, start, end):
+    if start < end:
+        p_index = partition(array, start, end)
+        quicksort(array, start, p_index-1)
+        quicksort(array, p_index+1, end)
+    return array
+
 def partition(array, start, end):
-    pivot = A[end]
+    pivot = array[end]
     # Set partition index as start initially
     p_index = start
     for i in range(start, end):
-        if A[i] <= pivot:
+        if array[i] <= pivot:
             # Swap if element is less than pivot
-            A[i], A[p_index] = A[p_index], A[i]
-            p_index++
+            array[i], array[p_index] = array[p_index], array[i]
+            p_index += 1
     # Swap pivot with element at partition index
-    A[p_index], A[end] = A[end], A[p_index]
+    array[p_index], array[end] = array[end], array[p_index]
     return p_index
+
+if __name__ == "__main__":
+    array = [7, 2, 1, 6, 8, 5, 3, 4]
+    print quicksort(array, 0, 7)
