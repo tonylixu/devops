daba974694125313325fd29e9975b10a1bf726b8
tonylixu@gmail.com <tonylixu@gmail.com>
2017-12-14 19:33:38 -0500

    Added test codes

--- algorithm/trees/binary-search-trees/solution.py
+++ algorithm/trees/binary-search-trees/solution.py
@@ -5,7 +5,7 @@ class Node:
 
 class Solution:
     def insert(self, root, data):
-        if root = None:
+        if root == None:
             return Node(data)
         else:
             if data <= root.data:
@@ -16,8 +16,17 @@ class Solution:
                 root.right = cur
         return root
 
-    def get_height(self.root):
+    def get_height(self, root):
         if root is None:
             return -1
         else:
             return max(self.get_height(root.left), self.get_height(root.right)) + 1
+
+T = int(raw_input())
+my_tree = Solution()
+root = None
+for i in range(T):
+    data = int(raw_input())
+    root = my_tree.insert(root, data)
+height = my_tree.get_height(root)
+print height
