f3754d423634a5f74e6ac16d994b9f7374c4600a
tonylixu@gmail.com <tonylixu@gmail.com>
2017-12-19 14:39:55 -0500

    A better version of solution1

--- algorithm/680-valid-palindrome-2/solution1.py
+++ algorithm/680-valid-palindrome-2/solution1.py
@@ -1,29 +1,17 @@
 def is_palindrome(s):
-    queue = []
-    stack = []
-    is_palindrome = True
-    for i in s:
-        # Push into queue and stack
-        queue.insert(0, i)
-        stack.insert(0, i)
-    for i in range(len(s)/2):
-        a = stack.pop(0)
-        b = queue.pop(-1)
-        if a != b:
-            is_palindrome = False
-            break
-    return is_palindrome
-
-def validate(s):
-    s2 = [x for x in s]
-    if is_palindrome(s2):
+    if s == s[::-1]:
         return True
     else:
-        for i in range(len(s2)):
-            temp = s2.pop(i)
+        return False
+
+def validate(s):
+    if is_palindrome(s):
+        return True
+    else:
+        for i in range(len(s)):
+            s2 = s[:i] + s[i+1:]
             if is_palindrome(s2):
                 return True
-            s2.insert(i, temp)
     return False
 
 if __name__ == '__main__':
